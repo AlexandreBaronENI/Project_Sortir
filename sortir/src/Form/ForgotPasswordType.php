bf54fdbd7e33e6d8a18774be395c40e8ca5b81e4
Alexandre <abaron@hellowork.com>
2021-03-15 17:01:07 +0100

    :sparkles: Ajout des views et controller pour le mdp oublié

---
+++ sortir/src/Form/ForgotPasswordType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participants;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForgotPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mail', EmailType::class, [
+                'label' => "Email"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
