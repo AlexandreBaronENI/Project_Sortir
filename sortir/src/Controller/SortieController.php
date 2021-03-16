4d821ced86b6185f3a60b065fabf7ed4163c9985
Alexandre <abaron@hellowork.com>
2021-03-16 18:09:15 +0100

    :sparkles: Ajout des méthodes pour les états et gestion des sorties

--- sortir/src/Controller/SortieController.php
+++ sortir/src/Controller/SortieController.php
@@ -8,6 +8,8 @@ use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Entity\Utilisateur;
 use App\Form\AddSortieType;
+use App\Form\EditSortieType;
+use App\Repository\LieuRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,37 +24,85 @@ class SortieController extends AbstractController
      */
     public function add(EntityManagerInterface $em, Request $request)
     {
-        {
-            $sortie = new Sortie();
-            $sortieForm = $this->createForm(AddSortieType::class, $sortie);
-            $sortieForm->handleRequest($request);
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(AddSortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
 
 
-            if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
-                if ($sortieForm->get('save')->isClicked()) {
-                    $etat = $em->getRepository(Etat::class)->findOneBy(['libelle' => 'draft']);
-                    $sortie->setOrganisateur($this->getUser());
-                    $sortie->setSite($this->getUser()->getSite());
-                    $sortie->setEtat($etat);
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            if ($sortieForm->get('save')->isClicked()) {
+                $etat = $em->getRepository(Etat::class)->findOneBy(['libelle' => 'draft']);
+                $sortie->setOrganisateur($this->getUser());
+                $sortie->setSite($this->getUser()->getSite());
+                $sortie->setEtat($etat);
 
-                    $em->persist($sortie);
-                    $em->flush();
-                }
-                elseif ($sortieForm->get('publish')->isClicked()) {
-                    $etat = $em->getRepository(Etat::class)->findOneBy(['libelle' => 'open']);
-                    $sortie->setOrganisateur($this->getUser());
-                    $sortie->setSite($this->getUser()->getSite());
-                    $sortie->setEtat($etat);
-
-                    $em->persist($sortie);
-                    $em->flush();
-                }
+                $em->persist($sortie);
+                $em->flush();
             }
+            elseif ($sortieForm->get('publish')->isClicked()) {
+                $etat = $em->getRepository(Etat::class)->findOneBy(['libelle' => 'open']);
+                $sortie->setOrganisateur($this->getUser());
+                $sortie->setSite($this->getUser()->getSite());
+                $sortie->setEtat($etat);
 
-
-            return $this->render("sortie/add.html.twig", [
-                "sortieForm" => $sortieForm->createView()
-            ]);
+                $em->persist($sortie);
+                $em->flush();
+            }
         }
+
+
+        return $this->render("sortie/add.html.twig", [
+            "sortieForm" => $sortieForm->createView()
+        ]);
+    }
+
+    /**
+     * Modifier une sortie
+     * @Route("/editsortie-{id}", name="editSortie", requirements={"id"="\d+"})
+     */
+    public function edit($id, EntityManagerInterface $em, Request $request)
+    {
+        $user = $em->getRepository(Utilisateur::class)->find(1);
+        $etatManager = new Etat($em->getRepository(Etat::class));
+
+        $sortie = $em->getRepository(Sortie::class)->find($id);
+        $sortieForm = $this->createForm(EditSortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        if ($sortie == null) {
+            throw $this->createNotFoundException('La sortie n\'a pas été trouvée !');
+        }
+
+        if($etatManager->IsClosed($sortie->getEtat()->getId()) || $etatManager->IsFinished($sortie->getEtat()->getId()) || $etatManager->IsCanceled($sortie->getEtat()->getId())){
+            throw $this->createNotFoundException('La sortie est terminée !');
+        }
+
+        if($sortie->getEtat() == $etatManager->IsActive($sortie->getEtat()->getId())){
+            throw $this->createNotFoundException('La sortie est en cours !');
+        }
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            if ($etatManager->IsDraft($sortie->getEtat()->getId()) && $sortieForm->get('save')->isClicked()) {
+                $em->persist($sortie);
+                $em->flush();
+            }
+            elseif ($etatManager->IsDraft($sortie->getEtat()->getId()) && $sortieForm->get('publish')->isClicked()) {
+                $etat = $etatManager->getOpen();
+                $sortie->setEtat($etat);
+
+                $em->persist($sortie);
+                $em->flush();
+            }
+            elseif (($etatManager->IsDraft($sortie->getEtat()->getId()) || $etatManager->IsOpen($sortie->getEtat()->getId())) && $sortieForm->get('delete')->isClicked()) {
+                $etat = $etatManager->getCanceled();
+                $sortie->setEtat($etat);
+                $em->persist($sortie);
+                $em->flush();
+            }
+        }
+
+        return $this->render("sortie/edit.html.twig", [
+            "sortieForm" => $sortieForm->createView()
+        ]);
     }
 }
