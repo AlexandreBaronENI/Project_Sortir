d68bbec69730bf9a4836de0b0907ec8ca91fc68f
Jean-Baptiste-Filleul <67331434+Jean-Baptiste-Filleul@users.noreply.github.com>
2021-03-16 19:27:53 +0100

    ProfilForm add

--- sortir/src/Controller/TestController.php
+++ sortir/src/Controller/TestController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Participants;
+use App\Form\ProfilType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'test')]
-    public function index(): Response
+
+    /**
+     * @Route("/test/add", name="test.add")
+     */
+    public function add(EntityManagerInterface $em, Request $request)
     {
+        $participant = new Participants();
+        $profilForm = $this->createForm(ProfilType::class, $participant);
+
+        $profilForm->handleRequest($request);
+        if ($profilForm->isSubmitted() && $profilForm->isValid()) {
+            $em->persist($participant);
+            $em->flush();
+            $this->addFlash('success', 'Votre profil a bien été sauvegardé !');
+            return $this->redirectToRoute('/forgotpassword', ['NoParticipant' => $participant->getNoParticipant()]);
+        }
+
         return $this->render('profile/profil.html.twig', [
-            'controller_name' => 'TestController',
+
+            "profilForm" => $profilForm->createView()
         ]);
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return $this->render("profile/profiltemp.html.twig");
     }
 }
--- sortir/src/Form/ProfilType.php
+++ sortir/src/Form/ProfilType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Participants;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,39 @@ class ProfilType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('mail')
-            ->add('password')
+            ->add('pseudo', TextType::class,
+                [ 'label' => 'Pseudo: ',
+                    'invalid_message' => 'Le pseudo est obligatoire',
+                    'required' => true,
+                ])
+            ->add('nom', TextType::class,
+                [ 'label' => 'Nom: ',
+                    'invalid_message' => 'Le nom est obligatoire',
+                    'required' => true,
+                ])
+            ->add('prenom', TextType::class,
+                [ 'label' => 'Prenom',
+                    'invalid_message' => 'Le prénom est obligatoire',
+                    'required' => true,
+                ])
+            ->add('telephone', TextType::class,
+                [ 'label' => 'Téléphone: ',
+                    'invalid_message' => 'Le numéro de téléphone est obligatoire',
+                    'required' => true,
+                ])
+            ->add('mail', EmailType::class,
+                [ 'label' => 'Email: ',
+                'invalid_message' => 'Le courriel est obligatoire',
+                'required' => true,
+                ])
+            ->add('password', RepeatedType::class,
+                [  'label' => 'Mot de passe: ',
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Le mot de passe est obligatoire',
+                    'required' => true,
+                    'first_options'  => array('label' => 'Mot de passe'),
+                    'second_options' => array('label' => 'Confirmation')
+                ])
             ->add('administrateur')
             ->add('actif')
             ->add('sitesNoSite')
