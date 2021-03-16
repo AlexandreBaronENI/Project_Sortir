5e48e4d9437978dd1accfcddeb7ad7f8c6212fef
ClementBrn <clement.breyne@gmail.com>
2021-03-16 13:44:54 +0100

    LoginPage

--- sortir/src/Form/LoginFormType.php
+++ sortir/src/Form/LoginFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LoginFormType extends AbstractType
@@ -18,6 +19,9 @@ class LoginFormType extends AbstractType
             ->add('username', TextType::class,
             ['label'=>'Identifiant'])
             ->add('password', PasswordType::class)
+            ->add('check', CheckboxType::class,[
+                'label'=> 'Se souvenir de moi : '
+            ])
             ->add('submit', SubmitType::class,
             ['label'=>'Connexion'])
         ;
