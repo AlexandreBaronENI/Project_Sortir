0ac1993bb1eb65c0aeab3919b7a5691d8ec7480a
ClementBrn <clement.breyne@gmail.com>
2021-03-19 14:17:05 +0100

    Gestion admin sortie + quelques corrections

--- sortir/src/Controller/AdminController.php
+++ sortir/src/Controller/AdminController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Lieu;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\AddLocationType;
 
 /**
  * @Route("/admin")
@@ -18,6 +21,28 @@ class AdminController extends AbstractController
     public function locations()
     {
         return $this->redirectToRoute('home');
+    }
+
+    /**
+     * Ajout d'un lieu
+     * @Route("/location/add", name="add-location")
+     */
+    public function addLocation(EntityManagerInterface $em, Request $request)
+    {
+        $lieu = new Lieu();
+        $locationForm = $this->createForm(addLocationType::class, $lieu);
+        $locationForm->handleRequest($request);
+        
+        if ($locationForm->isSubmitted() && $locationForm->isValid() ) {
+            $em->persist($lieu);
+            $em->flush();
+
+            $this->addFlash('success', 'Nouveau lieu ajouté !');
+        }
+
+        return $this->render('admin/location/add.html.twig',[
+            'locationForm' => $locationForm->createView()
+        ]);
     }
 
     /**
